70a46b154aec05b572b8de53a66b58ccbcbeb94c
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2024-01-25 22:58:41 +0300

    Connect mongodb

---
+++ mongoConnection.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', true)
+
+const main = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/blog-case')
+    console.log('Connected to MongoDB')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+main()
