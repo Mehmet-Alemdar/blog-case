8b08442903af9b061cb3f609c205414705563d1f
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2024-01-25 23:02:08 +0300

    Create blog schema

---
+++ src/models/blog.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const blogSchema = new Schema({
+  title: { type: String, required: true, trim: true, minlength: 2},
+  content: { type: String, required: true, trim: true, minlength: 3},
+  author: { type: Schema.Types.ObjectId, ref: 'User', required: true},
+  likes: { type: Schema.Types.ObjectId, ref: 'User', default: []},
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
