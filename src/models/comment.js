b035dbf5bd63f35f3b57c0175728605d5d006efb
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2024-01-26 23:25:09 +0300

    Add populate to comment schema

--- src/models/comment.js
+++ src/models/comment.js
@@ -4,8 +4,11 @@ const Schema = mongoose.Schema
 const commentSchema = new Schema({
   content: { type: String, required: true, trim: true, minlength: 1},
   createdAt: { type: Date, default: Date.now},
-  author: { type: Schema.Types.ObjectId, ref: 'User', required: true},
+  author: { type: Schema.Types.ObjectId, ref: 'User', required: true, autopopulate: { select: 'name profilePicture' }},
   blog: { type: Schema.Types.ObjectId, ref: 'Blog', required: true}
 })
+
+commentSchema.index({ '$**': 'text' })
+commentSchema.plugin(require('mongoose-autopopulate'))
 
 module.exports = mongoose.model('Comment', commentSchema)
