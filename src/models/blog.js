d316eb9e541b0218d5e394b7dfcef1d77fd7d8c9
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2024-01-26 22:19:44 +0300

    Add population to blog schema

--- src/models/blog.js
+++ src/models/blog.js
@@ -4,9 +4,12 @@ const Schema = mongoose.Schema
 const blogSchema = new Schema({
   title: { type: String, required: true, trim: true, minlength: 2},
   content: { type: String, required: true, trim: true, minlength: 3},
-  author: { type: Schema.Types.ObjectId, ref: 'User', required: true},
-  likes: { type: Schema.Types.ObjectId, ref: 'User', default: []},
+  author: { type: Schema.Types.ObjectId, ref: 'User', required: true, autopopulate: { select: 'name profilePicture email age' }},
+  likes: [{ type: Schema.Types.ObjectId, ref: 'User', default: []}],
   createdAt: { type: Date, default: Date.now},
 })
+
+blogSchema.index({ '$**': 'text' })
+blogSchema.plugin(require('mongoose-autopopulate'))
 
 module.exports = mongoose.model('Blog', blogSchema)
