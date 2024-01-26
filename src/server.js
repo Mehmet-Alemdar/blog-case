616c63a26d25b02625a4bf55f355ed61c3b7cfa4
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2024-01-26 10:10:24 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const  {errorHandler}  = require('./middlewares/errorHandler')
 require('../mongoConnection')
+const user = require('./routers/user')
 const app = express()
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.use('/user', user)
 app.use(errorHandler)
 
 module.exports = { app }
