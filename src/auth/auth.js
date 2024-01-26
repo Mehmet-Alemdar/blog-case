ce517227618b98126d6e12c2af5187a513ad062f
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2024-01-26 10:08:36 +0300

    Create authentication process

---
+++ src/auth/auth.js
@@ -0,0 +1,56 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+const comparePassword = (password, hash) => {
+  return bcrypt.compare(password, hash)
+}
+
+const hashPassword = (password) => {
+  return bcrypt.hash(password, 5)
+}
+
+const createJWT = (user) => {
+  const token = jwt.sign({
+    id: user.id,
+    email: user.email
+  }, "test secret", { expiresIn: '1m' })
+
+  return token
+}
+
+const protect = async (req, res, next) => {
+  const brearer = req.headers.authorization
+
+  if(!brearer) {
+    return res.status(401).json({
+      message: 'Unauthorized'
+    })
+  }
+
+  const[,token] = brearer.split(' ')
+
+  if(!token) {
+    res.status(401).json({
+      message: 'Not valid token'
+    })
+    return
+  }
+
+  try {
+    const user = jwt.verify(token, "test secret")
+    req.user = user
+    next()
+  }catch(err) {
+    res.status(401).json({
+      message: 'Not valid token'
+    })
+    return 
+  }
+}
+
+module.exports = {
+  comparePassword,
+  hashPassword,
+  createJWT,
+  protect
+}
