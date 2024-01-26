02c0875de4d1f79da8bdb6bd07fe58756a7218a7
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2024-01-26 10:10:12 +0300

    Make custom mongo error

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,11 @@
 const errorHandler = (err, req, res, next) => {
-  const errorStatus = err.status || 500
-  const errorMessage = err.message || 'Something broke!'
+  let errorStatus = err.status || 500
+  let errorMessage = err.message || 'Something broke!'
+
+  if(err.code === 11000) {
+    errorStatus = 400
+    errorMessage = 'Email already exists'
+  }
   
   res.status(errorStatus).send({
     status: errorStatus,
